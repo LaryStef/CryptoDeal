fc9d834d8990eab1ead01f4f60134398b9eecbd2
Lary_Stef <timurkotov1999@gmail.com>
2024-04-22 10:24:04 +0300

    fix: main page logo

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
 from .routes import main
-
+from .config import AppConfig
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(AppConfig)
     app.register_blueprint(main)
-
+    
     return app
