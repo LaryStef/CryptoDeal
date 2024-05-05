ecc0eac0e804caf79db0276f36a87ced6efa588f
Lary_Stef <timurkotov1999@gmail.com>
2024-05-05 22:35:57 +0300

    feat: env variables support

--- application/config.py
+++ application/config.py
@@ -1,6 +1,12 @@
+import os
+from dotenv import load_dotenv
+
 from flask import Config
 
+
+load_dotenv()
 
 class AppConfig(Config):
     DEBUG: bool = True
     SQLALCHEMY_DATABASE_URI: str = "postgresql://postgres:^Q06Rmt{@localhost:5432/postgres"
+    SECRET_KEY = os.getenv("SECRET_KEY")
