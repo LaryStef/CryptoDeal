e19433380ba255da031476a13af0625eb94b72e8
Lary_Stef <timurkotov1999@gmail.com>
2024-05-10 18:32:54 +0300

    feat: add sending form data to server

--- application/apis/auth.py
+++ application/apis/auth.py
@@ -1,4 +1,5 @@
 from flask_restx import Namespace, Resource
+from flask import request, make_response, jsonify
 
 
 api = Namespace("auth", path="/auth/")
@@ -7,4 +8,12 @@ api = Namespace("auth", path="/auth/")
 @api.route("/sign-up")
 class Sign_up(Resource):
     def post(self):
-        return "OK", 200
+        data = request.form
+        return make_response(jsonify(data), 200)
+
+
+@api.route("/sign-in")
+class Sign_in(Resource):
+    def post(self):
+        data = request.form
+        return make_response(jsonify(data), 200)
--- application/static/js/auth.js
+++ application/static/js/auth.js
@@ -1,11 +1,32 @@
-var url = new URL("api/auth/sign-up", location.origin);
+var loginUrl = new URL("api/auth/sign-in", location.origin);
+var registerUrl = new URL("api/auth/sign-up", location.origin);
+
 
 document.getElementById("login-form-id").addEventListener("submit", async (e) => {
     e.preventDefault();
 
     let formData = new FormData(document.getElementById("login-form-id"));
 
-    let response = await fetch(url, {
+    let response = await fetch(loginUrl, {
+        method: "POST",
+        credentials: "same-origin",
+        body: formData
+    });
+    
+    if (response.status == 200) {
+        let result = await response.json();
+        console.log(result);
+    } else {
+        console.log(response.status);
+    }
+})
+
+document.getElementById("register-form-id").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    let formData = new FormData(document.getElementById("register-form-id"));
+
+    let response = await fetch(registerUrl, {
         method: "POST",
         credentials: "same-origin",
         body: formData
