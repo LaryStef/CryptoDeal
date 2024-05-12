3289802bda7b6e1276e495017877c1fbf14af8be
Lary_Stef <timurkotov1999@gmail.com>
2024-05-12 22:53:41 +0300

    fix: braces

--- application/config.py
+++ application/config.py
@@ -10,9 +10,8 @@ class AppConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = f"postgresql://postgres:{os.getenv('DATABASE_PASSWORD')}@localhost:5432/postgres"
     SECRET_KEY = os.getenv("SECRET_KEY")
-    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 465
-    MAIL_USE_TLS = False
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.getenv("MAIL")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
