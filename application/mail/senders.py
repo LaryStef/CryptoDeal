e13802472625d1565d671a56fbb84f558b67e31b
Lary_Stef <timurkotov1999@gmail.com>
2024-06-08 23:22:22 +0300

    feat: add warning to email with code for password change

--- application/mail/senders.py
+++ application/mail/senders.py
@@ -18,12 +18,11 @@ def send_register_code(code: str, recipient: str) -> None:
 def send_restore_code(code: str, recipient: str) -> None:
     recipients = ["timurkotov1999@gmail.com"]   # just for tests
 
-    # TODO add ...if it isn't you... to message
-
     message = Message(f"Password restore on CryptoDeal", recipients=recipients)
     message.html = f"""
         <h2>Secret code: {code}<h2>
+        <p style="font-size: 16px; color: black">Warning: If it isn't you, immediately change password<p>
         <p style="font-size: 14px; color: black">Don't reply to this email<p>
         <p style="font-size: 14px; color: black">Support on Support@cryptodeal.com<p>
     """
-    mail.send(message)
+    # mail.send(message)
