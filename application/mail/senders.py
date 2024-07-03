3d561186fb3c4d48a9ecf05494758264a479a486
LaryStef <timurkotov1999@gmail.com>
2024-07-03 20:32:12 +0300

    feat: send scrf attention plug

--- application/mail/senders.py
+++ application/mail/senders.py
@@ -6,7 +6,7 @@ from . import mail
 def send_register_code(code: str, recipient: str) -> None:
     recipients: list[str] = ["timurkotov1999@gmail.com"]   # just for tests
   
-    message: Message = Message(f"Registration on CryptoDeal", recipients=recipients)
+    message: Message = Message("Registration on CryptoDeal", recipients=recipients)
     message.html = f"""
         <h2>Secret code: {code}<h2>
         <p style="font-size: 14px; color: black">Don't reply to this email<p>
@@ -18,7 +18,7 @@ def send_register_code(code: str, recipient: str) -> None:
 def send_restore_code(code: str, recipient: str) -> None:
     recipients: list[str] = ["timurkotov1999@gmail.com"]   # just for tests
 
-    message: Message = Message(f"Password restore on CryptoDeal", recipients=recipients)
+    message: Message = Message("Password restore on CryptoDeal", recipients=recipients)
     message.html = f"""
         <h2>Secret code: {code}<h2>
         <p style="font-size: 16px; color: black">Warning: If it isn't you, immediately change password<p>
@@ -26,3 +26,7 @@ def send_restore_code(code: str, recipient: str) -> None:
         <p style="font-size: 14px; color: black">Support on Support@cryptodeal.com<p>
     """
     mail.send(message)
+
+
+def send_scrf_attention(recipient: str) -> None:
+    pass
