2c8e87e2388e0d25f1fa18ddb74f0900045bf0b0
LaryStef <timurkotov1999@gmail.com>
2024-07-03 16:56:25 +0300

    feat: adding sessions to database

--- application/database/postgre/models.py
+++ application/database/postgre/models.py
@@ -1,22 +1,55 @@
-from sqlalchemy import Integer, String, Column, ForeignKey
+from sqlalchemy import Integer, String
 from sqlalchemy.orm import mapped_column, Mapped
 
 from . import db
 
+# TODO change datatype for timestamps in db to suitable datatype
+class User(db.Model):
+    def __init__(
+            self,
+            uuid: str,
+            name: str,
+            password_hash: str,
+            role: str,
+            email: str,
+            register_date: int,
+            restore_cooldown: int
+        ):
 
-class User(db.Model):    # type: ignore[name-defined]
-    def __init__(self, user_data: dict[str, str | int]):
-        self.uuid = user_data.get("uuid")    # type: ignore[assignment]
-        self.name: Mapped[str] = user_data.get("username")    # type: ignore[assignment]
-        self.password_hash: int = user_data.get("password_hash")    # type: ignore[assignment]
-        self.email: str | None = user_data.get("email")    # type: ignore[assignment]
-        self.register_date: int | None = user_data.get("register_date")    # type: ignore[assignment]
-        self.restore_cooldown: int | None = user_data.get("restore_cooldown")    # type: ignore[assignment]
-
+        self.uuid: str = uuid
+        self.name: str = name
+        self.password_hash: str = password_hash
+        self.role: str = role
+        self.email: str = email
+        self.register_date: int = register_date
+        self.restore_cooldown: int = restore_cooldown
 
     uuid: Mapped[str] = mapped_column(String(36), primary_key=True, unique=True)
     name: Mapped[str] = mapped_column(String(30))
     password_hash: Mapped[str] = mapped_column(String(64))
+    role: Mapped[str] = mapped_column(String(5), default="user")
     email: Mapped[str] = mapped_column(String(256), unique=True)
     register_date: Mapped[int] = mapped_column(Integer)
     restore_cooldown: Mapped[int] = mapped_column(Integer, default=0)
+
+
+class Session(db.Model):
+    uuid: Mapped[str] = mapped_column(String(36), primary_key=True, unique=True) # TODO foreign key
+    
+    session1: Mapped[str] = mapped_column(String(16), nullable=True)
+    session2: Mapped[str] = mapped_column(String(16), nullable=True)
+    session3: Mapped[str] = mapped_column(String(16), nullable=True)
+    session4: Mapped[str] = mapped_column(String(16), nullable=True)
+    session5: Mapped[str] = mapped_column(String(16), nullable=True)
+
+    device1: Mapped[str] = mapped_column(String, default="unnkown device")
+    device2: Mapped[str] = mapped_column(String, default="unnkown device")
+    device3: Mapped[str] = mapped_column(String, default="unnkown device")
+    device4: Mapped[str] = mapped_column(String, default="unnkown device")
+    device5: Mapped[str] = mapped_column(String, default="unnkown device")
+
+    activity1: Mapped[int] = mapped_column(Integer, default=0)
+    activity2: Mapped[int] = mapped_column(Integer, default=0)
+    activity3: Mapped[int] = mapped_column(Integer, default=0)
+    activity4: Mapped[int] = mapped_column(Integer, default=0)
+    activity5: Mapped[int] = mapped_column(Integer, default=0)
--- application/database/postgre/services.py
+++ application/database/postgre/services.py
@@ -1,9 +1,9 @@
-from typing import Any, NoReturn
-from time import time
+from typing import Any
+from datetime import datetime, UTC
 from uuid import uuid4
 
 from . import db
-from .models import User
+from .models import User, Session
 from ...config import AppConfig
 from ...utils.cryptography import hash_password
 
@@ -13,15 +13,51 @@ def get(table: db.Model, **kwargs: Any) -> Any | None:
 
 
 def add_user(user_data: dict[str, str | int]) -> None:
-    user_data["uuid"] = uuid4().__str__()
-    user_data["register_date"] = int(time())
+    _id = uuid4().__str__()
 
-    row: User = User(user_data)
-    db.session.add(row)
+    user: User = User(
+        uuid=_id,
+        name=user_data["username"],
+        password_hash=user_data["password_hash"],
+        role="user",
+        email=user_data["email"],
+        register_date=int(datetime.now(UTC).timestamp()),
+        restore_cooldown=0
+    )
+
+    db.session.add(user)
+    session: Session = Session(uuid=_id)
+    db.session.add(session)
+
     db.session.commit()
+    return _id
 
 
 def update_password(user: User, password: str) -> None:
-    user.restore_cooldown = int(time()) + AppConfig.RESTORE_COOLDOWN
+    user.restore_cooldown = int(datetime.now(UTC).timestamp()) + AppConfig.RESTORE_COOLDOWN
     user.password_hash = hash_password(password)
+    db.session.commit()
+
+
+def add_session(refresh_id: str, user_id: str, device: str) -> None:
+    sessions_raw: Session | None = get(Session, uuid=user_id)
+    if sessions_raw is None:
+        return
+    
+    sessions_data: dict[str, str] = sessions_raw.__dict__
+    
+    earlier_activity: float = float("inf")
+    for key, value in sessions_data.items():
+        if key.startswith("activity") and value < earlier_activity:
+            earlier_activity: int = value
+            oldest_session: str = key.replace("activity", "")
+    
+    setattr(sessions_raw, "session" + oldest_session, refresh_id)
+    setattr(sessions_raw, "activity" + oldest_session, datetime.now(UTC).timestamp())
+
+    if device is None:
+        setattr(sessions_raw, "device" + oldest_session, "unnkown device")
+    else:
+        setattr(sessions_raw, "device" + oldest_session, device)
+
     db.session.commit()
