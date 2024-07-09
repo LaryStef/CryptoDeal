cb83d46da945c7d360bbf2f380508980ca53e0f1
Lary_Stef <timurkotov1999@gmail.com>
2024-07-09 19:45:48 +0300

    feat: new session table model

--- application/database/postgre/models.py
+++ application/database/postgre/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import List
 
-from sqlalchemy import Integer, String, TIMESTAMP
+from sqlalchemy import Integer, String, TIMESTAMP, ForeignKey
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from . import db
@@ -9,23 +9,6 @@ from .timestamp import utcnow
 
 
 class User(db.Model):
-    def __init__(
-            self,
-            uuid: str,
-            name: str,
-            password_hash: str,
-            role: str,
-            email: str,
-            alien_number: int
-        ):
-
-        self.uuid: Mapped[str] = uuid
-        self.name: Mapped[str] = name
-        self.password_hash: Mapped[str] = password_hash
-        self.role: Mapped[str] = role
-        self.email: Mapped[str] = email
-        self.alien_number: Mapped[int] = alien_number
-
     __tablename__ = "user"
 
     uuid: Mapped[str] = mapped_column(String(36), primary_key=True)
@@ -38,11 +21,33 @@ class User(db.Model):
     alien_number: Mapped[int] = mapped_column(Integer, default=0)
     session: Mapped[List["Session"]] = relationship()
 
+    def __init__(
+        self,
+        uuid: str,
+        name: str,
+        password_hash: str,
+        role: str,
+        email: str,
+        alien_number: int
+    ) -> None:
+        
+        self.uuid: Mapped[str] = uuid
+        self.name: Mapped[str] = name
+        self.password_hash: Mapped[str] = password_hash
+        self.role: Mapped[str] = role
+        self.email: Mapped[str] = email
+        self.alien_number: Mapped[int] = alien_number
+
 
 class Session(db.Model):
     __tablename__ = "session"
 
-    session_id: Mapped[str] = mapped_column(String(36))
-    user_id: Mapped[str] = mapped_column(primary_key=True, ForeignKey="user.uuid")
+    session_id: Mapped[str] = mapped_column(String(16), primary_key=True)
+    user_id: Mapped[str] = mapped_column(ForeignKey("user.uuid"))
     device: Mapped[str] = mapped_column(String(30), default="unknown device")
     last_activity: Mapped[datetime] = mapped_column(TIMESTAMP, server_default=utcnow(), onupdate=utcnow())
+
+    def __init__(self, session_id: str, user_id: str, device: str):
+        self.session_id: Mapped[str] = session_id,
+        self.user_id: Mapped[str] = user_id,
+        self.device: Mapped[str] = device
