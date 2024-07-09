6658b7339233eab35cdda95efbddbe7e2983bd6f
Lary_Stef <timurkotov1999@gmail.com>
2024-07-09 19:47:05 +0300

    chore: font size decreased

--- application/mail/senders.py
+++ application/mail/senders.py
@@ -9,8 +9,8 @@ def send_register_code(code: str, recipient: str) -> None:
     message: Message = Message("Registration on CryptoDeal", recipients=recipients)
     message.html = f"""
         <h2>Secret code: {code}<h2>
-        <p style="font-size: 14px; color: black">Don't reply to this email<p>
-        <p style="font-size: 14px; color: black">Support on Support@cryptodeal.com<p>
+        <p style="font-size: 12px; color: black">Don't reply to this email<p>
+        <p style="font-size: 12px; color: black">Support on Support@cryptodeal.com<p>
     """
     mail.send(message)
 
@@ -22,8 +22,8 @@ def send_restore_code(code: str, recipient: str) -> None:
     message.html = f"""
         <h2>Secret code: {code}<h2>
         <p style="font-size: 16px; color: black">Warning: If it isn't you, immediately change password<p>
-        <p style="font-size: 14px; color: black">Don't reply to this email<p>
-        <p style="font-size: 14px; color: black">Support on Support@cryptodeal.com<p>
+        <p style="font-size: 12px; color: black">Don't reply to this email<p>
+        <p style="font-size: 12px; color: black">Support on Support@cryptodeal.com<p>
     """
     mail.send(message)
 
