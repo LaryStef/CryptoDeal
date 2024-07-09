3b828e81637dbe0dfe066a734fd71d98718391e4
Lary_Stef <timurkotov1999@gmail.com>
2024-07-09 14:45:07 +0300

    feat: session table relationship with user table

--- application/database/postgre/models.py
+++ application/database/postgre/models.py
@@ -1,10 +1,13 @@
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import mapped_column, Mapped
+from datetime import datetime
+from typing import List
 
+from sqlalchemy import Integer, String, TIMESTAMP
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from . import db
+from .timestamp import utcnow
 
-# TODO change datatype for timestamps in db to suitable datatype
+
 class User(db.Model):
     def __init__(
             self,
@@ -13,47 +16,33 @@ class User(db.Model):
             password_hash: str,
             role: str,
             email: str,
-            register_date: int,
-            restore_cooldown: int,
             alien_number: int
         ):
 
-        self.uuid: str = uuid
-        self.name: str = name
-        self.password_hash: str = password_hash
-        self.role: str = role
-        self.email: str = email
-        self.register_date: int = register_date
-        self.restore_cooldown: int = restore_cooldown
-        self.alien_number: int = alien_number
+        self.uuid: Mapped[str] = uuid
+        self.name: Mapped[str] = name
+        self.password_hash: Mapped[str] = password_hash
+        self.role: Mapped[str] = role
+        self.email: Mapped[str] = email
+        self.alien_number: Mapped[int] = alien_number
 
-    uuid: Mapped[str] = mapped_column(String(36), primary_key=True, unique=True)
+    __tablename__ = "user"
+
+    uuid: Mapped[str] = mapped_column(String(36), primary_key=True)
     name: Mapped[str] = mapped_column(String(30))
     password_hash: Mapped[str] = mapped_column(String(64))
     role: Mapped[str] = mapped_column(String(5), default="user")
     email: Mapped[str] = mapped_column(String(256), unique=True)
-    register_date: Mapped[int] = mapped_column(Integer, nullable=True)
-    restore_cooldown: Mapped[int] = mapped_column(Integer, default=0)
+    register_date: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=True, server_default=utcnow())
+    restore_cooldown: Mapped[datetime] = mapped_column(TIMESTAMP, server_default=utcnow(), onupdate=utcnow())
     alien_number: Mapped[int] = mapped_column(Integer, default=0)
+    session: Mapped[List["Session"]] = relationship()
 
 
 class Session(db.Model):
-    uuid: Mapped[str] = mapped_column(String(36), primary_key=True, unique=True) # TODO foreign key
-    
-    session1: Mapped[str] = mapped_column(String(16), nullable=True)
-    session2: Mapped[str] = mapped_column(String(16), nullable=True)
-    session3: Mapped[str] = mapped_column(String(16), nullable=True)
-    session4: Mapped[str] = mapped_column(String(16), nullable=True)
-    session5: Mapped[str] = mapped_column(String(16), nullable=True)
+    __tablename__ = "session"
 
-    device1: Mapped[str] = mapped_column(String(30), default="unnkown device")
-    device2: Mapped[str] = mapped_column(String(30), default="unnkown device")
-    device3: Mapped[str] = mapped_column(String(30), default="unnkown device")
-    device4: Mapped[str] = mapped_column(String(30), default="unnkown device")
-    device5: Mapped[str] = mapped_column(String(30), default="unnkown device")
-
-    activity1: Mapped[int] = mapped_column(Integer, default=0)
-    activity2: Mapped[int] = mapped_column(Integer, default=0)
-    activity3: Mapped[int] = mapped_column(Integer, default=0)
-    activity4: Mapped[int] = mapped_column(Integer, default=0)
-    activity5: Mapped[int] = mapped_column(Integer, default=0)
+    session_id: Mapped[str] = mapped_column(String(36))
+    user_id: Mapped[str] = mapped_column(primary_key=True, ForeignKey="user.uuid")
+    device: Mapped[str] = mapped_column(String(30), default="unknown device")
+    last_activity: Mapped[datetime] = mapped_column(TIMESTAMP, server_default=utcnow(), onupdate=utcnow())
