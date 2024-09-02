a688f137958ad857525cafa645c5d2f68bafae5f
LaryStef <timurkotov1999@gmail.com>
2024-09-02 21:27:48 +0300

    fix: crypto api response

--- app/apis/crypto.py
+++ app/apis/crypto.py
@@ -17,6 +17,36 @@ CurrencyData: t.TypeAlias = list[dict[str, str | int]]
 @api.route("/list")
 class List(Resource):
     def get(self) -> tuple[int | dict[str, CurrencyData]]:
+        # response example
+        # {
+        #     "CryptoCurrencyList": [
+        #         {
+        #             "name": "Ethereum",
+        #             "ticker": "ETH",
+        #             "logo": "/static/png/cryptocurrency/Ethereum",
+        #             "price": 2682.823913,
+        #             "volume": 7433271729.333189,
+        #             "change": -6.240546710846617
+        #         },
+        #         {
+        #             "name": "Bitcoin",
+        #             "ticker": "BTC",
+        #             "logo": "/static/png/cryptocurrency/Bitcoin",
+        #             "price": 72916.728154,
+        #             "volume": 28821829110.47828,
+        #             "change": 8.263753024401588
+        #         },
+        #         {
+        #             "name": "Tether",
+        #             "ticker": "USDT",
+        #             "logo": "/static/png/cryptocurrency/Tether",
+        #             "price": 1.000523,
+        #             "volume": 91738219.0,
+        #             "change": 0.014194591860516859
+        #         }
+        #     ]
+        # }
+
         currencies: ScalarResult[CryptoCurrency] = get(
             table=CryptoCurrency,
             many=True
@@ -30,12 +60,11 @@ class List(Resource):
                 ticker=currency.ticker,
                 time_frame="hour" + str(hour + 1)
             )
-            last_course: ScalarResult[CryptoCourse] = get(
+            new_course: ScalarResult[CryptoCourse] = get(
                 table=CryptoCourse,
                 ticker=currency.ticker,
                 time_frame="hour" + str(hour)
             )
-            change: float = (last_course.price / course_day_ago.price - 1)*100
 
             currency_list.append(
                 {
@@ -45,32 +74,12 @@ class List(Resource):
                         "static",
                         filename=f"png/cryptocurrency/{currency.name}"
                     ),
-                    "price": last_course.price,
+                    "price": new_course.price,
                     "volume": currency.volume,
-                    "change": "+" if change >= 0 else "" + change,
-                    "posTrend": change >= 0
+                    "change": (new_course.price / course_day_ago.price - 1)*100
                 }
             )
 
         return {
             "CryptoCurrencyList": currency_list
         }, 200
-
-
-# data = {
-#     "CryptoCurrencyList": [
-#         {
-#             "name": "",
-#             "ticker": "",
-#             "logo": "",
-#             "price": 0,
-#             "volume": 0,
-#             "change": 0
-#         }
-#     ]
-# }
-
-
-# lst = list(range(9))
-# new_lst = [lst[i] if i % 2 == 0 else lst[-i] for i in range(len(lst))]
-# print(new_lst)
