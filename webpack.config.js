05857598a6a74b11b2350a52301b3a746fcb0d03
LaryStef <timurkotov1999@gmail.com>
2024-09-24 13:25:56 +0300

    chore: turn off creating license for js bundles

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,9 @@ module.exports = {
     },
     optimization: {
         minimize: true,
-        minimizer: [new TerserPlugin()],
+        minimizer: [new TerserPlugin({
+            "extractComments": false,
+        })],
     },
     mode: "production",
 };
