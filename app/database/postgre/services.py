5ee14913eb7c89ad7ea1d8e500b8d9d38a20b3ff
Lary_Stef <timurkotov1999@gmail.com>
2024-09-25 20:08:12 +0300

    refactor: handler for postgre services

--- app/database/postgre/services.py
+++ app/database/postgre/services.py
@@ -11,110 +11,111 @@ from app.logger import logger
 from app.utils.cryptography import hash_password
 
 
-# TODO add postgre handler
-def get(
-    table: Any = None,
-    fields: list[Mapped] = [],
-    *,
-    many: bool = False,
-    **kwargs: Any
-) -> Any:
-    if table is not None:
+class PostgreHandler:
+    @staticmethod
+    def get(
+        table: Any = None,
+        fields: list[Mapped] = [],
+        *,
+        many: bool = False,
+        **kwargs: Any
+    ) -> Any:
+        if table is not None:
+            result: Result = db.session.execute(
+                select(table).filter_by(**kwargs)
+            )
+            if many:
+                return result.scalars()
+            return result.scalar()
+
         result: Result = db.session.execute(
-            select(table).filter_by(**kwargs)
+            select(*fields).filter_by(**kwargs)
         )
+
         if many:
             return result.scalars()
-        return result.scalar()
+        return result.fetchone()
 
-    result: Result = db.session.execute(
-        select(*fields).filter_by(**kwargs)
-    )
+    @staticmethod
+    def remove(table: Any, **kwargs: Any):
+        db.session.execute(
+            delete(table).filter_by(**kwargs)
+        )
+        db.session.commit()
+        logger.info(msg=f"deleted raw/s with args: {kwargs} in {table}")
 
-    if many:
-        return result.scalars()
-    return result.fetchone()
+    @staticmethod
+    def delete_exclude(
+        table: Any,
+        column: Mapped[Any],
+        exclude: list[Any],
+        **kwargs: Any
+    ):
+        db.session.execute(
+            delete(table).where(column.not_in(exclude)).filter_by(**kwargs)
+        )
+        db.session.commit()
+        logger.info(msg=f"""
+            deleted all raw/s exclude raws: {kwargs} in {column} in {table}
+        """)
 
+    @staticmethod
+    def add_user(user_data: dict[str, str | int]) -> tuple[str, int]:
+        id_: str = uuid4().__str__()
+        alien_number: int = randint(1, 5)
 
-def remove(table: Any, **kwargs: Any):
-    db.session.execute(
-        delete(table).filter_by(**kwargs)
-    )
-    db.session.commit()
-    logger.info(msg=f"deleted raw/s with args: {kwargs} in {table}")
+        user: User = User(
+            uuid=id_,
+            name=user_data["username"],
+            password_hash=user_data["password_hash"],
+            role=user_data["role"],
+            email=user_data["email"],
+            alien_number=alien_number
+        )
+        fiat_wallet: FiatWallet = FiatWallet(
+            ID=uuid4().__str__(),
+            user_id=id_
+        )
 
+        db.session.add(user)
+        db.session.add(fiat_wallet)
+        db.session.commit()
+        logger.info(msg=f"added {user_data['role']} {user_data['username']}")
+        return id_, alien_number
 
-def delete_exclude(
-    table: Any,
-    column: Mapped[Any],
-    exclude: list[Any],
-    **kwargs: Any
-):
-    db.session.execute(
-        delete(table).where(column.not_in(exclude)).filter_by(**kwargs)
-    )
-    db.session.commit()
-    logger.info(msg=f"""
-        deleted all raw/s exclude raws: {kwargs} in {column} in {table}
-    """)
+    @staticmethod
+    def update_password(user: User, password: str) -> None:
+        user.password_hash = hash_password(password)
+        user.restore_date = utcnow()
+        db.session.commit()
+        logger.info(msg=f"updated password {user.name}")
 
+    @staticmethod
+    def add_session(refresh_id: str, user_id: str, device: str) -> None:
+        session_raw: Session = Session(
+            session_id=refresh_id,
+            user_id=user_id,
+            device=device
+        )
+        db.session.add(session_raw)
+        db.session.commit()
+        logger.info(msg=f"new session added {user_id} on {device}")
 
-def add_user(user_data: dict[str, str | int]) -> tuple[str, int]:
-    id_: str = uuid4().__str__()
-    alien_number: int = randint(1, 5)
+    @staticmethod
+    def update_session(
+            old_refresh_id: str,
+            new_refresh_id: str,
+            user_id: str,
+            device: str
+    ) -> None:
+        session_raw: Session | None = db.session.query(Session).filter_by(
+            user_id=user_id,
+            session_id=old_refresh_id
+        ).first()
+        if session_raw is None:
+            return
 
-    user: User = User(
-        uuid=id_,
-        name=user_data["username"],
-        password_hash=user_data["password_hash"],
-        role=user_data["role"],
-        email=user_data["email"],
-        alien_number=alien_number
-    )
-    fiat_wallet: FiatWallet = FiatWallet(
-        ID=uuid4().__str__(),
-        user_id=id_
-    )
-
-    db.session.add(user)
-    db.session.add(fiat_wallet)
-    db.session.commit()
-    logger.info(msg=f"added {user_data['role']} {user_data['username']}")
-    return id_, alien_number
-
-
-def update_password(user: User, password: str) -> None:
-    user.password_hash = hash_password(password)
-    user.restore_date = utcnow()
-    db.session.commit()
-    logger.info(msg=f"updated password {user.name}")
-
-
-def add_session(refresh_id: str, user_id: str, device: str) -> None:
-    session_raw: Session = Session(
-        session_id=refresh_id,
-        user_id=user_id,
-        device=device
-    )
-    db.session.add(session_raw)
-    db.session.commit()
-    logger.info(msg=f"new session added {user_id} on {device}")
-
-
-def update_session(
-        old_refresh_id: str,
-        new_refresh_id: str,
-        user_id: str,
-        device: str
-) -> None:
-    session_raw: Session | None = db.session.query(Session).filter_by(
-        user_id=user_id,
-        session_id=old_refresh_id
-    ).first()
-    if session_raw is None:
-        return
-
-    session_raw.session_id = new_refresh_id
-    session_raw.device = device
-    db.session.commit()
-    logger.info(msg=f"session refreshed {user_id}")
+        session_raw.session_id = new_refresh_id
+        session_raw.device = device
+        db.session.commit()
+        logger.info(msg=f"session refreshed {user_id}")
