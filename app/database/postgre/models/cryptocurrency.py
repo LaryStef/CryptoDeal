1523344cd823a01d60a06701531235b18e657862
LaryStef <timurkotov1999@gmail.com>
2024-09-27 20:01:54 +0300

    chore

--- app/database/postgre/models/cryptocurrency.py
+++ app/database/postgre/models/cryptocurrency.py
@@ -20,7 +20,7 @@ class CryptoCurrency(db.Model):
     volume: Mapped[float] = mapped_column(Float, default=0)
     crypto_course: Mapped[list["CryptoCourse"]] = relationship()
     curr_wallet: Mapped["CryptocurrencyWallet"] = relationship(
-        backref="CryptocurrencyWallet.cryptocurrency"
+        backref="cryptocurrency"
     )
 
     def __init__(
