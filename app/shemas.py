d2ebf65d4f1af3f2ca429aeb65143388bb849180
Lary_Stef <timurkotov1999@gmail.com>
2024-10-02 14:14:59 +0300

    feat: schemas for transaction api

--- app/shemas.py
+++ app/shemas.py
@@ -24,3 +24,18 @@ class LoginSchema(Schema):
         required=True
     )
     device: Field = fields.Str(validate=validate.Length(6, 30))
+
+
+class TransactionSchema(Schema):
+    amount: Field = fields.Integer(
+        validate=validate.Range(min=0, max=4_294_967_296)
+    )
+    type_: Field = fields.Str(validate=validate.OneOf(choices=["buy", "sell"]))
+
+
+class CryptoTransactionSchema(TransactionSchema):
+    ticker: Field = fields.Str(required=True, validate=validate.Length(3, 8))
+
+
+class FiatTransactionSchema(TransactionSchema):
+    iso: Field = fields.Str(required=True, validate=validate.Length(3, 8))
