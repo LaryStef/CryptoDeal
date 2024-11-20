b761f3b863b7c8c29a6abf7cafe0da6785445c77
LaryStef <timurkotov1999@gmail.com>
2024-11-20 16:39:25 +0300

    chore: minor changes

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,6 @@ from app.database.redisdb import rediska
 from app.mail import mail
 from app.routes import main
 from app.tasks import celery_init_app
-from app.utils.push_test_data import push_cryptocurrencies  # noqa F401
 
 
 def create_app() -> tuple[Flask, Celery]:
@@ -25,13 +24,14 @@ def create_app() -> tuple[Flask, Celery]:
 
     celery: Celery = celery_init_app(app)
 
+    if rediska.json().get("register") is None:
+        rediska.json().set(name="register", path=".", obj={})
+    if rediska.json().get("password_restore") is None:
+        rediska.json().set(name="password_restore", path=".", obj={})
+
     with app.app_context():
+        from app.utils.push_test_data import push_cryptocurrencies  # noqa F401
         # db.create_all()
         # push_cryptocurrencies()
-
-        if rediska.json().get("register") is None:
-            rediska.json().set(name="register", path=".", obj={})
-        if rediska.json().get("password_restore") is None:
-            rediska.json().set(name="password_restore", path=".", obj={})
 
     return app, celery
