ec54ed394845a309eabb368760e944ef9631a491
LaryStef <timurkotov1999@gmail.com>
2024-11-21 23:39:25 +0300

    feat: call setup_logging when app starting

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from app.database.redisdb import rediska
 from app.mail import mail
 from app.routes import main
 from app.tasks import celery_init_app
+from app.logger import setup_logging
 
 
 def create_app() -> tuple[Flask, Celery]:
@@ -15,6 +16,7 @@ def create_app() -> tuple[Flask, Celery]:
     app.config.from_object(appConfig)
     app.register_blueprint(main)
 
+    setup_logging(app)
     db.init_app(app)
     api.init_app(app)
     mail.init_app(app)
@@ -29,9 +31,9 @@ def create_app() -> tuple[Flask, Celery]:
     if rediska.json().get("password_restore") is None:
         rediska.json().set(name="password_restore", path=".", obj={})
 
-    with app.app_context():
-        from app.utils.push_test_data import push_cryptocurrencies  # noqa F401
-        # db.create_all()
-        # push_cryptocurrencies()
+    # with app.app_context():
+    #     from app.utils.push_test_data import push_cryptocurrencies
+    #     db.create_all()
+    #     push_cryptocurrencies()
 
     return app, celery
