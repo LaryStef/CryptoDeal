31f44f2a75cc05465d16d0fd47ae77f122866fec
LaryStef <timurkotov1999@gmail.com>
2024-11-21 23:41:31 +0300

    feat: setup celery loggers after starting app

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -1,5 +1,8 @@
-from celery import Celery, Task
+from logging import Formatter
+
 from flask import Flask
+from celery import Celery, Task
+from celery.signals import after_setup_logger, after_setup_task_logger
 
 
 def celery_init_app(app: Flask) -> Celery:
@@ -13,3 +16,23 @@ def celery_init_app(app: Flask) -> Celery:
     celery_app.set_default()
     app.extensions["celery"] = celery_app
     return celery_app
+
+
+@after_setup_logger.connect
+def setup_celery_logger(logger, *args, **kwargs):
+    formatter: Formatter = Formatter(
+        fmt="[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s",  # noqa: E501
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    for handler in logger.handlers:
+        handler.setFormatter(formatter)
+
+
+@after_setup_task_logger.connect
+def setup_celery_task_logger(logger, *args, **kwargs):
+    formatter: Formatter = Formatter(
+        fmt="[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s",  # noqa: E501
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    for handler in logger.handlers:
+        handler.setFormatter(formatter)
