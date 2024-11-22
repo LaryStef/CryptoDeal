560c1a6bb875d1bdf366d9b0983512b4bd145727
LaryStef <timurkotov1999@gmail.com>
2024-11-22 13:07:38 +0300

    feat: run scripts

--- run.py
+++ run.py
@@ -8,5 +8,5 @@ app: Flask
 celery: Celery
 app, celery = create_app()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
