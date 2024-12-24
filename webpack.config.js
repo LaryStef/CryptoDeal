cd3041104f23a3d8cdea54b5de557bed34dc1e84
LaryStef <timurkotov1999@gmail.com>
2024-12-24 21:40:18 +0300

    feat: Webpack configuration for css compilation from scss files

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,33 @@
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 
 module.exports = {
-    //watch: true,
+    watch: true,
     entry: {
-        index: "./frontend/js/index.js",
         cryptoList: "./frontend/js/cryptoList.js",
+        index: "./frontend/js/index.js",
         profile: "./frontend/js/profile.js",
         cryptocurrency: "./frontend/js/cryptocurrency.js"
     },
     output: {
-        path: path.resolve(__dirname, "./app/static/js/"),
-        filename: "[name].min.js"
+        path: path.resolve(__dirname, "./app/static"),
+        filename: "./js/[name].min.js"
     },
     resolve: {
         extensions: [".js"],
+    },
+    plugins: [new MiniCssExtractPlugin({
+        filename: "css/[name].min.css",
+    })],
+    module: {
+      rules: [
+        {
+            test: /\.s[ac]ss$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+      ],
     },
     optimization: {
         minimize: true,
