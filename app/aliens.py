45621f3bf5066192cf32764197fc9e1b495ae4e3
lary_stef <timurkotov1999@gmail.com>
2025-02-07 23:12:10 +0300

    feat: Aliens colors TypedDict

---
+++ app/aliens.py
@@ -0,0 +1,32 @@
+from typing import TypedDict
+
+
+class AlienColors(TypedDict):
+    Alien1: str
+    Alien2: str
+    Alien3: str
+    Alien4: str
+    Alien5: str
+    Alien6: str
+    Alien7: str
+    Alien8: str
+    Alien9: str
+    Alien10: str
+    Alien11: str
+    Alien12: str
+
+
+alien_colors: AlienColors = AlienColors(
+    Alien1="#404cb8",
+    Alien2="#8fff06",
+    Alien3="#664993",
+    Alien4="#38054a",
+    Alien5="#f758b7",
+    Alien6="#b04e33",
+    Alien7="#cc9764",
+    Alien8="#f8ee24",
+    Alien9="#204917",
+    Alien10="#218064",
+    Alien11="#b13333",
+    Alien12="#afb58f"
+)
